114800dbd38b8dc3bdc4be35c562a71d4610077d
John <gjohncarter@protonmail.com>
2022-09-02 10:04:14 -0700

    submission

--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const app = espress();
+const app = express();
 
-require('../config/mongoose.config');
+require('./config/mongoose.config');
 
 app.use(express.json(), express.urlencoded({ extended: true }));
 
-const AllMyJokeRoutes = require('./routes/joke.routes');
+const AllMyJokeRoutes = require('./routes/jokes.routes');
 AllMyJokeRoutes(app);
 
 app.listen(8000, () => console.log("We are a green light on poert 8000"));
